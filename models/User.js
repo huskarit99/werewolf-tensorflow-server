f6572b0b017e3cdb5fda5a4a0a55be7b3a592b50
Duy Ha <hacaoduy161@gmail.com>
2021-02-02 11:51:51 +0700

    add: User models

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+    },
+    username: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    token: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("user", UserSchema);
