46869fec35e89a44851bfef4d62d153d7336929f
huskarit99 <nguyenthaihocxm1999@gmail.com>
2021-05-20 22:00:25 +0700

    ADD: leave room feature

--- listeners/react.leave-room.js
+++ listeners/react.leave-room.js
@@ -1,6 +1,15 @@
+import jwt from 'jsonwebtoken';
+import userRepository from '../data/repositories/user.repository.js';
+
 export default (io, socket, listRoom, rooms, checkUserInRoom) => {
-  socket.on("react:leave-room", ({ id, usernameOfPlayer }) => {
+  socket.on("react:leave-room", async ({ id, token }) => {
     // Conditional: User leave room
+    if (!token) return;
+    // Identify and get info user by token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.user.id;
+    const user = await userRepository.getUserById(userId);
+    const usernameOfPlayer = user.username;
     // Find index of room in list room
     const indexRoom = listRoom.findIndex(room => room.id === id);
     // Check whether user who leaves room is host or not 
@@ -13,8 +22,8 @@ export default (io, socket, listRoom, rooms, checkUserInRoom) => {
       // Remove this player from room
       const indexPlayerinRoom = rooms[id].member.findIndex(player => player.username === usernameOfPlayer);
       rooms[id].member.splice(indexPlayerinRoom, 1);
-      // Notify all players in the same room
-      socket.to(id).emit("server:update-room", rooms);
+      // Notify all players in the same rooms
+      socket.to(id).emit("server:update-room", rooms[id]);
       // Update number of players in room
       listRoom[indexRoom].numberOfPlayersInRoom = rooms[id].member.length;
     }
